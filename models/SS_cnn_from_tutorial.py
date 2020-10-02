7da94878bbf03ce27fcb5f6365730ccf9e76761b
Henrietta Holze <henrietta.holze@embl.de>
2020-10-02 19:38:56 +0200

    first working version of SS CNN based on tutorials, 8 classes, mostly only predicts one class

---
+++ models/SS_cnn_from_tutorial.py
@@ -0,0 +1,171 @@
+# TODO
+# translate labels into 3 categories
+# figure out what the 8 categories are biologically
+# comment and clean code
+# evaluate accuracy on train set
+# evaluate accuracy validation set
+# categories are heavily biased -> only 1st and 6th category are ever predicted
+
+import numpy as np 
+import os
+
+import torch
+from torch.nn import Module, Sequential, Conv1d, ReLU, MaxPool1d, Linear, CrossEntropyLoss, Dropout
+from torch.optim import SGD
+import torch.nn.functional as F
+from torch.autograd import Variable
+from sklearn.metrics import accuracy_score, confusion_matrix,classification_report
+
+# Load and reshape data (see explore_data.py)
+DATA_DIR = '/home/henri/Repos/python_for_data_science/group_project_2nd_struct/ICML2014'
+data = np.load(os.path.join(DATA_DIR, 'cullpdb+profile_5926.npy'))
+
+data_reshaped = data.reshape(5926, 700, -1)
+
+X_train = data_reshaped[0:5430, :, np.r_[0:21, 31:33, 35:57]]
+y_train = data_reshaped[0:5430, :, 22:30]
+X_test = data_reshaped[5435:5690, :, np.r_[0:21, 31:33, 35:57]]
+y_test = data_reshaped[5435:5690, :, 22:30]
+X_val = data_reshaped[5690:5926, :, np.r_[0:21, 31:33, 35:57]]
+y_val = data_reshaped[5690:5926, :, 22:30]
+
+print(X_train.shape)
+# (5430, 700, 45)       # proteins, aa, features
+
+print(y_train.shape)
+# (5430, 700, 8)
+
+X_train = torch.from_numpy(X_train).float()
+y_train = torch.from_numpy(y_train)
+X_val = torch.from_numpy(X_val).float()
+y_val = torch.from_numpy(y_val)
+
+BATCH_SIZE = 30
+# also possible: 181
+
+train = torch.utils.data.TensorDataset(X_train, y_train)
+val = torch.utils.data.TensorDataset(X_val, y_val)
+
+train_loader = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val, batch_size=BATCH_SIZE, shuffle=True)
+
+# Create CNN Model
+class CnnModel(Module):
+    def __init__(self):
+        super(CnnModel, self).__init__()
+        # in_channel == number of features (1 in b/w images, 3 in colored images, 45 in our case)
+        # out_channel == number of filters to learn
+        # kernel_size == window/filter size
+        # padding = kernel_size / 2 - 1 to conserve sequence length
+        self.conv1 = Conv1d(in_channels=45, out_channels=16, kernel_size=9, padding=4)
+        # 8 output categories
+        self.conv2 = Conv1d(in_channels=16, out_channels=8, kernel_size=3, padding=1)
+    def forward(self, x):
+        # conv functions need as input: (batchsize, channels, sequence)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        return x
+
+
+# defining the model
+print('creating model')
+model = CnnModel()
+print(model)
+
+# Hyperparameters
+num_epochs = 15
+learning_rate = 0.001
+
+# defining optimizer
+optimizer = SGD(model.parameters(), lr=learning_rate)
+
+# Cross Entropy Loss
+criterion = CrossEntropyLoss()
+
+
+# Train
+print('training model')
+for e in range(num_epochs):
+    running_loss = 0
+    for i, (sequences, labels) in enumerate(train_loader):
+        # the sequences of each iterable of the DataLoader object have dimensions (batchsize, 700, 45)
+        # but the conv layers need (batchsize, channels, sequence) as input
+        # -> transpose second and third dimension
+        # print('train shape before transposition:', sequences.shape)
+        sequences = torch.transpose(sequences, 1, 2)
+        train = Variable(sequences)
+        # print('train shape after transposition', train.shape)
+        # reverse one-hot encoding to integers 0-7, so that labels are of shape (batchsize, sequence)
+        # needed for loss function 
+        labels = np.argmax(labels, axis=2)
+        labels = Variable(labels)
+        # print('labels shape', labels.shape)
+        # --------------------------------------------------------
+        # Clear gradients
+        optimizer.zero_grad()
+        # Forward propagation
+        outputs = model(train)
+        # Calculate softmax and CEL
+        # shape required: (batchsize, number of classes, sequence), (batchsize, sequence)
+        loss = criterion(outputs, labels)
+        # Calculating gradients
+        loss.backward()
+        # Update parameters
+        optimizer.step()
+        # 
+        running_loss += loss
+    else:
+        print("Epoch {} - Training loss: {}".format(e, running_loss / len(train_loader)))
+
+
+#########################################################################################
+# Evaluation
+#########################################################################################
+
+############### on training set
+
+# pass train set through the model
+with torch.no_grad():
+    output = model(torch.transpose(X_train, 1, 2))
+
+# get predictions
+softmax = torch.exp(output)
+prob = list(softmax.numpy())
+predictions = np.argmax(prob, axis=1)
+
+# get labels
+labels_train = np.argmax(y_train, axis=2)
+
+# get overall accuracy
+acc_train = accuracy_score(labels_train.reshape(-1), predictions.reshape(-1))
+print('accuracy train', acc_train)
+
+
+############### on validation set
+
+# pass val set through model
+with torch.no_grad():
+    output = model(torch.transpose(X_val, 1, 2))
+
+# get predictions
+softmax = torch.exp(output)
+prob = list(softmax.numpy())
+predictions_val = np.argmax(prob, axis=1)
+pred_val_unrolled = predictions_val.reshape(-1)
+
+# get labels
+labels_val = np.argmax(y_val, axis=2)
+labels_val_unrolled = labels_val.reshape(-1)
+
+# get overall accuracy
+acc_val = accuracy_score(labels_val_unrolled, pred_val_unrolled)
+print('accuracy val', acc_val)
+
+# get confusion matrix for all classes (tp, tn, fp, fn)
+print(confusion_matrix(labels_val_unrolled, pred_val_unrolled))
+
+# see precision, recall, f1 score and support for all classes
+target_names = ['class 0', 'class 1', 'class 2', 'class 3', 'class 4', 'class 5', 'class 6', 'class 7']
+print(classification_report(labels_val_unrolled, pred_val_unrolled, target_names=target_names))
