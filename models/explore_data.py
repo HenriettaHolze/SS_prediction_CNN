c2f1dd9691a43bb26f79e16f0cf0e604d681ac5d
Henrietta Holze <szr214@alumni.ku.dk>
2020-11-05 21:45:50 +0100

    use NoSeq label

--- models/explore_data.py
+++ models/explore_data.py
@@ -40,38 +40,46 @@ print(data_reshaped.shape)
 # The last feature of both amino acid residues and secondary structure labels just mark end of the protein sequence. 
 # [22,31) and [33,35) are hidden during testing.
 
-# NoSeq features removed
-X_train = data_reshaped[0:5430, :, np.r_[0:21, 31:33, 35:57]]
-y_train = data_reshaped[0:5430, :, 22:30]
-X_test = data_reshaped[5435:5690, :, np.r_[0:21, 31:33, 35:57]]
-y_test = data_reshaped[5435:5690, :, 22:30]
-X_val = data_reshaped[5690:5926, :, np.r_[0:21, 31:33, 35:57]]
-y_val = data_reshaped[5690:5926, :, 22:30]
+# Include NoSeq label!!!
+# all example sequences are 700 positions long but the proteins aren't, so the trailing positions are labeled with NoSeq
+# including the label but not optimizing on it should resolve class bias problem 
+
+X_train = data_reshaped[0:5430, :, np.r_[0:22, 31:33, 35:57]]
+y_train = data_reshaped[0:5430, :, 22:31]
+X_test = data_reshaped[5435:5690, :, np.r_[0:22, 31:33, 35:57]]
+y_test = data_reshaped[5435:5690, :, 22:31]
+X_val = data_reshaped[5690:5926, :, np.r_[0:22, 31:33, 35:57]]
+y_val = data_reshaped[5690:5926, :, 22:31]
 
 print(X_train.shape)
-# (5430, 700, 45)       # proteins, aa, features
+# (5430, 700, 46)       # proteins, aa, features
 
 print(y_train.shape)
-# (5430, 700, 8)
+# (5430, 700, 9)
 
+# now has 9 labels instead of 8
 
 ###########################
 # distribution of classes
 ###########################
 
-all_labels = data_reshaped[:, :, 22:30]
-all_labels = all_labels.reshape(-1, 8)
+# after including NoSeq, the distribution is way more even 
+# does a weighted loss function still make sense? NoSeq should be ignored and only the bias in the other classes should be considered
+
+all_labels = data_reshaped[:, :, 22:31]
+all_labels = all_labels.reshape(-1, 9)
 all_labels = np.argmax(all_labels, axis=1)
 
 label_distribution = Counter(all_labels)
-# Counter({0: 3142553, 5: 429890, 2: 270087, 7: 140813, 6: 103017, 3: 48643, 1: 12970, 4: 227})
+# Counter({8: 2902726, 5: 429890, 2: 270087, 0: 239827, 7: 140813, 6: 103017, 3: 48643, 1: 12970, 4: 227})
 
 # calculating weights for loss function
 # Weight of class c is the size of largest class divided by the size of class c. 
 # convert counter object to ordered list
-label_distribution = [label_distribution[i] for i in range(len(label_distribution))]
+# ignore NoSeq class
+label_distribution = [label_distribution[i] for i in range(len(label_distribution) - 1)]
 
-largest_class = label_distribution.max()
+largest_class = max(label_distribution)
 
 loss_weights = [largest_class / label_distribution[i] for i in range(len(label_distribution))]
 
@@ -89,20 +97,22 @@ loss_weights = [largest_class / label_distribution[i] for i in range(len(label_d
 # S: 23, 24
 # H: 25, 26, 27
 
-data_reshaped_Q3 = data_reshaped
+data_reshaped_Q3 = data_reshaped.copy()
 data_reshaped_Q3[22] = data_reshaped_Q3[22] + data_reshaped_Q3[28] + data_reshaped_Q3[29]
 data_reshaped_Q3[23] = data_reshaped_Q3[23] + data_reshaped_Q3[24] 
 data_reshaped_Q3[24] = data_reshaped_Q3[25] + data_reshaped_Q3[26] + data_reshaped_Q3[27]
+data_reshaped_Q3[25] = data_reshaped_Q3[30]
 
-X_train_Q3 = data_reshaped[0:5430, :, np.r_[0:21, 31:33, 35:57]]
-y_train_Q3 = data_reshaped[0:5430, :, 22:25]
-X_test_Q3 = data_reshaped[5435:5690, :, np.r_[0:21, 31:33, 35:57]]
-y_test_Q3 = data_reshaped[5435:5690, :, 22:25]
-X_val_Q3 = data_reshaped[5690:5926, :, np.r_[0:21, 31:33, 35:57]]
-y_val_Q3 = data_reshaped[5690:5926, :, 22:25]
+X_train_Q3 = data_reshaped_Q3[0:5430, :, np.r_[0:22, 31:33, 35:57]]
+y_train_Q3 = data_reshaped_Q3[0:5430, :, 22:26]
+X_test_Q3 = data_reshaped_Q3[5435:5690, :, np.r_[0:22, 31:33, 35:57]]
+y_test_Q3 = data_reshaped_Q3[5435:5690, :, 22:26]
+X_val_Q3 = data_reshaped_Q3[5690:5926, :, np.r_[0:22, 31:33, 35:57]]
+y_val_Q3 = data_reshaped_Q3[5690:5926, :, 22:26]
 
-all_labels = data_reshaped_Q3[:, :, 22:25]
-all_labels = all_labels.reshape(-1, 3)
+all_labels = data_reshaped_Q3[:, :, 22:26]
+# now 4 classes (including NoSeq class)
+all_labels = all_labels.reshape(-1, 4)
 all_labels = np.argmax(all_labels, axis=1)
 
 print(Counter(all_labels))
@@ -114,14 +124,14 @@ print(Counter(all_labels))
 ###############################################
 
 # we have input data in these dimensions -> flag the corners of the matrix to see what happens during loading the data
-X_train = torch.ones(5430, 700, 45).float()
-y_train = torch.ones(5430, 700, 8)
+X_train = torch.ones(5430, 700, 46).float()
+y_train = torch.ones(5430, 700, 9)
 BATCH_SIZE = 30
 
 X_train[0, 0, 0] = 0
 X_train[0, 699, 0] = 2
-X_train[0, 0, 44] = 3
-X_train[0, 699, 44] = 4
+X_train[0, 0, 45] = 3
+X_train[0, 699, 45] = 4
 
 # we use the DataLoader and define a batch size
 train = torch.utils.data.TensorDataset(X_train, y_train)
@@ -136,10 +146,10 @@ for i, (sequences, labels) in enumerate(train_loader):
 
 # the flags are in their correct position
 train[0, 0, 0]
-train[0, 699, 44]
+train[0, 699, 45]
 
 # for feeding into the model, we need (BATCH_SIZE, channels, sequence) so we transpose the last 2 dimensions
 train_trans = torch.transpose(train, 1, 2)
 print(train_trans.shape)
 print(train_trans[0, 0])
-print(train[0, 44])
+print(train[0, 45])
